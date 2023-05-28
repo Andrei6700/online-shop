1e5dbbc0f0583692c943d20de220282f977eec60
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-05-29 01:49:53 +0300

    Initial commit

---
+++ src/UI/pagina-main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./pagina-main.css";
+import { Header } from "./components/header";
+import { Filters } from "./components/filters";
+import { Content } from "./components/content";
+import { Footer } from "./components/footer";
+
+export const PaginaMain=()=>{
+    return  (
+        <div className="layout">
+            <Header/>
+            <Filters/>
+            <Content/>
+            <Footer/>
+
+
+        </div>
+    )
+}
