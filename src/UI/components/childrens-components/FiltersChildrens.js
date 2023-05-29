533ab9739313760b4b12e78b5f35ac26f9f4e9e8
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-05-30 02:14:54 +0300

    Update project files

--- src/UI/components/childrens-components/FiltersChildrens.js
+++ src/UI/components/childrens-components/FiltersChildrens.js
@@ -18,6 +18,7 @@ export const FiltersChildrens = () => {
   return (
     <div className="layout-filtre">
       <div>
+        <p>Tara de origine</p>
         <input
           type="checkbox"
           name="SUA"
@@ -62,6 +63,60 @@ export const FiltersChildrens = () => {
         />
         <label htmlFor="ROMANIA">ROMANIA</label>
       </div>
+     
+      <p>Tip Tanck</p>
+      <div>
+        <input
+          type="checkbox"
+          name="Light Tanck"
+          checked={checkedItems["Light Tanck"] || false}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="CHINA">Light Tanck</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="heavy tanks"
+          checked={checkedItems["heavy tanks"] || false}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="heavy tanks">heavy tanks</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="medium tanks"
+          checked={checkedItems["medium tanks"] || false}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="medium tanks">medium tanks</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="SPAA"
+          checked={checkedItems["SPAA"] || false}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="SPAA">SPAA</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="tank destroyers"
+          checked={checkedItems["tank destroyers"] || false}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="tank destroyers">tank destroyers</label>
+      </div>
+
+
+
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );
