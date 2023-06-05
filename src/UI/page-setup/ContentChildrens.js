d4b779abcda18cf6e404f7e967d0debc390a9cf0
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-06-05 21:43:52 +0300

    code splitting

--- src/UI/page-setup/ContentChildrens.js
+++ src/UI/page-setup/ContentChildrens.js
@@ -1,193 +1,25 @@
 import React from "react"
-import ADATS from "../tancuri-img/ADATS.jpg"
-import CCVL from "../tancuri-img/CCVL.jpg"
-import HSTV_L from "../tancuri-img/HSTV_L.jpg"
-import IT_1 from "../tancuri-img/IT_1.jpeg"
-import Leopard_2A6 from "../tancuri-img/Leopard_2A6.jpg"
-import M1_Abrams from "../tancuri-img/M1_Abrams.jpg"
-import M103 from "../tancuri-img/M103.jpeg"
-import Ozelot from "../tancuri-img/Ozelot.jpeg"
-import PUMA from "../tancuri-img/PUMA.jpg"
-import SOMUA_SM from "../tancuri-img/SOMUA_SM.jpg"
-import T_80U from "../tancuri-img/T_80U.jpeg"
-import TAM_2C from "../tancuri-img/TAM_2C.jpeg"
-import Tiger_2 from "../tancuri-img/Tiger_2.jpeg"
-import Tiger_E from "../tancuri-img/Tiger_E.jpeg"
-import WIESEL_1A from "../tancuri-img/WIESEL_1A.jpg"
-import XM975 from "../tancuri-img/XM975.png"
 import "./ContentChildrens.css"
 import MainPage from "../../UI-Form/MainPage"
 import { Link } from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { tankData } from "./tank-data/TankData"
 
 
 export const ContentChildrens = () => {
   return (
     <div className="layout-content">
-      <a href="/MainPage" className="image-item">
-          <img src={M103} alt="M103" style={{ objectFit: "cover" }} />
+      {tankData.map((tank) => (
+        <a href="/MainPage" className="image-item" key={tank.name}>
+          <img src={tank.image} alt={tank.name} style={{ objectFit: "cover" }} />
           <div className="item-info">
-            <p>M103</p>
-            <p>TEXT</p>
-            <p>12.500.300$</p>
+            <p>{tank.name}</p>
+            <p>{tank.text}</p>
+            <p>{tank.price}</p>
             <button>Buy</button>
           </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={Ozelot} alt="Ozelot" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>Ozelot</p>
-          <p>TEXT</p>
-          <p>3.599.000$</p>
-          <button>Buy</button>
-        </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={PUMA} alt="PUMA" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>PUMA</p>
-          <p>TEXT</p>
-          <p>8.500.000$</p>
-          <button>Buy</button>
-        </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={SOMUA_SM} alt="SOMUA_SM" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>SOMUA SM</p>
-          <p>TEXT</p>
-          <p>1.500.000$</p>
-          <button>Buy</button>
-        </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={ADATS} alt="ADATS" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>ADATS</p>
-          <p>TEXT</p>
-          <p>8.500.000$</p>
-          <button>Buy</button>
-        </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={CCVL} alt="CCVL" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>CCVL</p>
-          <p>TEXT</p>
-          <p>5.500.000$</p>
-          <button>Buy</button>
-        </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={HSTV_L} alt="HSTV_L" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>HSTV L</p>
-          <p>TEXT</p>
-          <p>6.000.000$</p>
-          <button>Buy</button>
-        </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={IT_1} alt="IT_1" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>IT 1</p>
-          <p>TEXT</p>
-          <p>8.500.000$</p>
-          <button>Buy</button>
-        </div>
-      </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={Leopard_2A6} alt="Leopard_2A6" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>Leopard 2A6</p>
-          <p>TEXT</p>
-          <p>6.790.000$</p>
-          <button>Buy</button>
-        </div>
-    </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={M1_Abrams} alt="M1_Abra" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>Tiger E</p>
-          <p>Tanc Tiger E, German, 100mm</p>
-          <p>15.000.300$</p>
-          <button>Buy</button>
-        </div>
-    </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={TAM_2C} alt="TAM_2C" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>TAM 2</p>
-          <p> Text</p>
-          <p>18.500.000$</p>
-          <button>Buy</button>
-        </div>
-    </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={Tiger_2} alt="Tiger_2" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>Tiger E</p>
-          <p>Tanc Tiger II, German, 100mm</p>
-          <p>14.500.000$</p>
-          <button>Buy</button>
-        </div>
-
-    </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={WIESEL_1A} alt="WIESEL_1A" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>WIESEL 1A</p>
-          <p>text</p>
-          <p>2.500.000$</p>
-          <button>Buy</button>
-        </div>
-
-    </a>
-
-    <a href="//MainPage" className="image-item">
-        <img src={XM975} alt="XM975" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>XM975</p>
-          <p>text</p>
-          <p>7.000.000$</p>
-          <button>Buy</button>
-        </div>
-
-    </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={Tiger_E} alt="Tiger_E" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>Tiger E</p>
-          <p>Tanc Tiger E, German, 100mm</p>
-          <p>2.300.000$</p>
-          <button>Buy</button>
-        </div>
-
-    </a>
-
-      <a href="//MainPage" className="image-item">
-        <img src={T_80U} alt="T_80U" style={{ objectFit: "cover" }} />
-        <div className="item-info">
-          <p>T-80 U</p>
-          <p> Text </p>
-          <p>14.000.000$</p>
-          <button>Buy</button>
-        </div>
-
-    </a>
+        </a>
+      ))}
     </div>
-
-  )
-}
+  );
+};
---
+++ src/UI/page-setup/tank-data/TankData.js
@@ -0,0 +1,117 @@
+import React from "react"
+import ADATS from "../../../tancuri-img/ADATS.jpg"
+import CCVL from "../../../tancuri-img/CCVL.jpg"
+import HSTV_L from "../../tancuri-img/HSTV_L.jpg"
+import IT_1 from "../../tancuri-img/IT_1.jpeg"
+import Leopard_2A6 from "../../tancuri-img/Leopard_2A6.jpg"
+import M1_Abrams from "../../tancuri-img/M1_Abrams.jpg"
+import M103 from "../../tancuri-img/M103.jpeg"
+import Ozelot from "../../tancuri-img/Ozelot.jpeg"
+import PUMA from "../../tancuri-img/PUMA.jpg"
+import SOMUA_SM from "../../tancuri-img/SOMUA_SM.jpg"
+import T_80U from "../../tancuri-img/T_80U.jpeg"
+import TAM_2C from "../../tancuri-img/TAM_2C.jpeg"
+import Tiger_2 from "../../tancuri-img/Tiger_2.jpeg"
+import Tiger_E from "../../tancuri-img/Tiger_E.jpeg"
+import WIESEL_1A from "../../tancuri-img/WIESEL_1A.jpg"
+import XM975 from "../../tancuri-img/XM975.png"
+
+export const tankData = [
+    {
+      image: require("../tancuri-img/M103.jpeg"),
+      name: "M103",
+      text: "TEXT",
+      price: "12.500.300$",
+    },
+    {
+      image: require("../tancuri-img/Ozelot.jpeg"),
+      name: "Ozelot",
+      text: "TEXT",
+      price: "3.599.000$",
+    },
+    {
+      image: require("../tancuri-img/PUMA.jpg"),
+      name: "PUMA",
+      text: "TEXT",
+      price: "8.500.000$",
+    },
+    {
+      image: require("../tancuri-img/SOMUA_SM.jpg"),
+      name: "SOMUA SM",
+      text: "TEXT",
+      price: "1.500.000$",
+    },
+    {
+      image: require("../tancuri-img/ADATS.jpg"),
+      name: "ADATS",
+      text: "TEXT",
+      price: "8.500.000$",
+    },
+    {
+      image: require("../tancuri-img/CCVL.jpg"),
+      name: "CCVL",
+      text: "TEXT",
+      price: "5.500.000$",
+    },
+    {
+      image: require("../tancuri-img/HSTV_L.jpg"),
+      name: "HSTV L",
+      text: "TEXT",
+      price: "6.000.000$",
+    },
+    {
+      image: require("../tancuri-img/IT_1.jpeg"),
+      name: "IT 1",
+      text: "TEXT",
+      price: "8.500.000$",
+    },
+    {
+      image: require("../tancuri-img/Leopard_2A6.jpg"),
+      name: "Leopard 2A6",
+      text: "TEXT",
+      price: "6.790.000$",
+    },
+    {
+      image: require("../tancuri-img/M1_Abrams.jpg"),
+      name: "M1 Abrams",
+      text: "TEXT",
+      price: "15.000.300$",
+    },
+    {
+      image: require("../tancuri-img/TAM_2C.jpeg"),
+      name: "TAM 2C",
+      text: "TEXT",
+      price: "18.500.000$",
+    },
+    {
+      image: require("../tancuri-img/Tiger_2.jpeg"),
+      name: "Tiger II",
+      text: "TEXT",
+      price: "14.500.000$",
+    },
+    {
+      image: require("../tancuri-img/WIESEL_1A.jpg"),
+      name: "WIESEL 1A",
+      text: "TEXT",
+      price: "2.500.000$",
+    },
+    {
+      image: require("../tancuri-img/XM975.png"),
+      name: "XM975",
+      text: "TEXT",
+      price: "7.000.000$",
+    },
+    {
+      image: require("../tancuri-img/Tiger_E.jpeg"),
+      name: "Tiger E",
+      text: "TEXT",
+      price: "2.300.000$",
+    },
+    {
+      image: require("../tancuri-img/T_80U.jpeg"),
+      name: "T-80 U",
+      text: "TEXT",
+      price: "14.000.000$",
+    },
+  ];
+  
