0b50f2184190bc8f5327143ffb333c7cc421152c
DESKTOP-MV0FBBS\Hp <andreibalanoiu67@gmail.com>
2023-06-16 16:22:48 +0300

    Update children prop

--- src/UI/page-layout/content.js
+++ src/UI/page-layout/content.js
@@ -1,13 +1,10 @@
-import React from "react"
-import {ContentChildrens} from "../page-setup/ContentChildrens"
+import React from "react";
+import { ContentChildrens } from "../page-setup/ContentChildrens";
 
-export const Content=()=>{
-    return (
-        <div class="content">
-            <div className="title">Magazin Online de Tancuri
-            <hr className="hr-title"/>
-            </div>
-            <ContentChildrens/>
-        </div>
-    )
-}
+export const Content = () => {
+  return (
+    <div>
+      <ContentChildrens />
+    </div>
+  );
+};
--- src/UI/page-setup/ContentChildrens.js
+++ src/UI/page-setup/ContentChildrens.js
@@ -5,31 +5,43 @@ import { tankData } from "./tank-data/TankData";
 
 export const ContentChildrens = () => {
   return (
-    <div className="layout-content">
-      {tankData.map((tank) => (
-        <Link to={`/second/${tank.name}`} className="image-item" key={tank.name}>
-          <img src={tank.image} alt={tank.name} style={{ objectFit: "cover" }} />
-          <div className="item-info">
+    <div class="content">
+      <div className="title">
+        Magazin Online de Tancuri
+        <hr className="hr-title" />
+      </div>
+      <div className="layout-content">
+        {tankData.map((tank) => (
+          <Link
+            to={`/second/${tank.name}`}
+            className="image-item"
+            key={tank.name}
+          >
+            <img
+              src={tank.image}
+              alt={tank.name}
+              style={{ objectFit: "cover" }}
+            />
+            <div className="item-info">
+              <div>
+                <p className="produs">{tank.name}</p>
+              </div>
 
-            <div>
-              <p className="produs">{tank.name}</p>
-            </div>
+              <div>
+                <p className="text">{tank.text_description}</p>
+              </div>
 
-            <div>
-              <p className="text">{tank.text_description}</p>
-            </div>
+              <div>
+                <p className="price">{tank.price}</p>
+              </div>
 
-            <div>
-              <p className="price">{tank.price}</p>
+              <div>
+                <button className="button">Buy</button>
+              </div>
             </div>
-
-            <div>
-              <button className="button">Buy</button>
-            </div>
-            
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
